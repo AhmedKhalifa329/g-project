7db713b88bae6e32579dd2d5c95f54b4b4e1dbfe
AhmedKhalifa329 <Ahmedkhalifa2587@outlook.com>
2024-06-01 13:35:46 +0300

    test
    test for git

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -3,6 +3,7 @@
   <div class="about">
     <div class="continer">
       <h1 class="headr">This is {{ name }} page</h1>
+      <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ab hic, quo tenetur sit libero explicabo, quae ex sunt eveniet eius, quia atque iure incidunt numquam. Nesciunt eos amet laboriosam.</P>
     </div>
     <ProudcutView />
   </div>
