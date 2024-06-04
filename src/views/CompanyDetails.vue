2bc76a191c94556d4448f6c6a76df8a3940229f4
AhmedKhalifa329 <Ahmedkhalifa2587@outlook.com>
2024-06-04 14:59:12 +0300

    last edit

--- src/views/CompanyDetails.vue
+++ src/views/CompanyDetails.vue
@@ -2,12 +2,17 @@
 <template>
     <section>
         <div class="container">
-            <h1 class="text-center">{{ name }}</h1>
+            <h1 v-if="errorMsg">{{ errorMsg }}</h1>
+            <h1 class="text-center">{{ company.name }}</h1>
             <div class="row">
                 <div class="col-md-5">
                     <!-- img-->
                     <div class="Product-img">
-                        <img :src="image" class="img-fluid w-100 rounded-top" alt="" />
+                        <img
+                            :src="company.image"
+                            class="img-fluid w-100 rounded-top"
+                            alt=""
+                        />
                     </div>
                 </div>
                 <div class="col-md-7">
@@ -19,43 +24,81 @@
                         <ul class="list-unstyled">
                             <li class="mb-4">
                                 <i class="fas fa-phone fa-fw mr-4"></i>
-                                <span>Phone:</span>
-                                <a href="tel:+20123456789" class="text-link">+20123456789</a>
+                                <span>Phone :-</span>
+                                <a href="tel:+20123456789" class="text-link">
+                                    +20123456789 {{ company.phone }}</a
+                                >
                             </li>
                             <li class="mb-4">
                                 <i class="fas fa-envelope fa-fw mr-4"></i>
-                                <span>Email:</span>
-                                <a href="mailto:info@example.com" class="text-link">info@example.com</a>
+                                <span>Email :-</span>
+                                <a
+                                    href="mailto:info@example.com"
+                                    class="text-link"
+                                >
+                                    info@example.com{{ company.email }}</a
+                                >
                             </li>
                             <li class="mb-4">
                                 <i class="fas fa-globe fa-fw mr-2"></i>
-                                <span>Website:</span>
-                                <a href="https://www.example.com" target="_blank" class="text-link">www.example.com</a>
+                                <span>Website :-</span>
+                                <a
+                                    href="https://www.example.com"
+                                    target="_blank"
+                                    class="text-link"
+                                >
+                                    www.example.com{{ company.website }}</a
+                                >
                             </li>
                             <li class="mb-4">
                                 <i class="fas fa-map-marker-alt fa-fw mr-2"></i>
-                                <span>Address:</span>
-                                <p class="mb-0">Main Street, Building 123, Nasr City, Cairo</p>
+                                <span>Address :-</span>
+                                <p class="mb-0">
+                                    Main Street, Building 123, Nasr City,
+                                    Cairo{{ company.address }}
+                                </p>
                             </li>
                             <li class="mb-4">
                                 <div class="social-media">
                                     <div class="col-lg-8">
-                                        <div class="d-flex justify-content-end pt-3">
-                                            <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle a-icon"
-                                                href="">
-                                                <font-awesome-icon icon="fa-brands fa-x-twitter" class="icon" />
+                                        <div
+                                            class="d-flex justify-content-end pt-3"
+                                        >
+                                            <a
+                                                class="btn btn-outline-secondary me-2 btn-md-square rounded-circle a-icon"
+                                                href=""
+                                            >
+                                                <font-awesome-icon
+                                                    icon="fa-brands fa-x-twitter"
+                                                    class="icon"
+                                                />
                                             </a>
-                                            <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle a-icon"
-                                                href="">
-                                                <font-awesome-icon icon="fa-brands fa-facebook" class="icon" />
+                                            <a
+                                                class="btn btn-outline-secondary me-2 btn-md-square rounded-circle a-icon"
+                                                href=""
+                                            >
+                                                <font-awesome-icon
+                                                    icon="fa-brands fa-facebook"
+                                                    class="icon"
+                                                />
                                             </a>
-                                            <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle a-icon"
-                                                href="">
-                                                <font-awesome-icon icon="fa-brands fa-youtube" class="icon" />
+                                            <a
+                                                class="btn btn-outline-secondary me-2 btn-md-square rounded-circle a-icon"
+                                                href=""
+                                            >
+                                                <font-awesome-icon
+                                                    icon="fa-brands fa-youtube"
+                                                    class="icon"
+                                                />
                                             </a>
-                                            <a class="btn btn-outline-secondary btn-md-square rounded-circle a-icon"
-                                                href="">
-                                                <font-awesome-icon icon="fa-brands fa-linkedin-in" class="icon" />
+                                            <a
+                                                class="btn btn-outline-secondary btn-md-square rounded-circle a-icon"
+                                                href=""
+                                            >
+                                                <font-awesome-icon
+                                                    icon="fa-brands fa-linkedin-in"
+                                                    class="icon"
+                                                />
                                             </a>
                                         </div>
                                     </div>
@@ -71,15 +114,30 @@
 <!-- eslint-disable prettier/prettier-->
 
 <script>
-import JsonProducts from "../Json/product.json";
+import axios from "axios";
 export default {
     name: "CompanyDetails",
     data() {
         return {
-            Products: JsonProducts,
-            image: JsonProducts[0].image,
-            name: JsonProducts[0].name,
+            company: [],
+            errorMsg: "",
         };
+    },
+    methods: {
+        getcompany() {
+            axios
+                .get(`http://localhost:8000/products/${this.page}`)
+                .then((response) => {
+                    this.company = response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.errorMsg = "Error retrieving data";
+                });
+        },
+    },
+    created() {
+        this.getcompany();
     },
 };
 </script>
@@ -99,7 +157,7 @@ section {
     padding-top: 5rem;
     padding-bottom: 5rem;
 
-    color: #f5f0f0
+    color: #f5f0f0;
 }
 
 .text-link {
